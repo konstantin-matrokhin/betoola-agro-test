727fcdb31c3bb3a2cca3dd5494c39194d37f8bd0
Konstantin Matrokhin <remillary@gmail.com>
2021-05-15 19:50:42 +0700

    add Company entity

---
+++ src/main/java/ru/kmatrokhin/betoolatest/company/dao/Company.java
@@ -0,0 +1,42 @@
+package ru.kmatrokhin.betoolatest.company.dao;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import ru.kmatrokhin.betoolatest.cultivation.dao.Cultivation;
+
+import javax.persistence.*;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "company")
+@EqualsAndHashCode(of = "id")
+public class Company {
+  @Id
+  @GeneratedValue
+  private UUID id;
+
+  @Column(name = "address")
+  private String address;
+
+  @Column(name = "city")
+  private String city;
+
+  @OneToMany(mappedBy = "company", fetch = FetchType.LAZY)
+  private Set<Cultivation> cultivations;
+
+  @Column(name = "country")
+  private String country;
+
+  @Column(name = "fiscal_id")
+  private String fiscalId;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "postal_code")
+  private String postalCode;
+}
