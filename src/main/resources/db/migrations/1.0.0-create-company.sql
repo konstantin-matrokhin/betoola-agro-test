3b45a4c982aca4bce9b0afa9beaee05a8557229d
Konstantin Matrokhin <remillary@gmail.com>
2021-05-15 19:49:58 +0700

    fix sql in migrations

--- src/main/resources/db/migrations/1.0.0-create-company.sql
+++ src/main/resources/db/migrations/1.0.0-create-company.sql
@@ -3,11 +3,11 @@
 --changeset Konstantin Matrokhin:1
 create table company
 (
-    id          uuid not null default random_uuid(),
+    id          uuid         not null default random_uuid(),
     address     varchar(128),
-    city        varchar(128),
+    city        varchar(3),
     country     varchar(128) not null,
     fiscal_id   varchar(10),
     name        varchar(128),
-    postal_code varchar(10) not null
+    postal_code varchar(10)  not null
 );
--- src/main/resources/db/migrations/1.0.1-create-cultivation.sql
+++ src/main/resources/db/migrations/1.0.1-create-cultivation.sql
@@ -3,13 +3,13 @@
 --changeset Konstantin Matrokhin:1
 create table cultivation
 (
-    id                  uuid                  default random_uuid(),
+    id                  uuid         not null default random_uuid(),
     name                varchar(128),
     accounting_year     int,
     cultivated_variety  varchar(128) not null,
-    company_id          uuid references company (id),
-    created             timestamp    not null default now(),
-    modified            timestamp    not null default now(),
+    company_id          uuid         not null references company (id),
+    created_date        timestamp    not null default now(),
+    modified_date       timestamp    not null default now(),
     polygon_coordinates json,
     polygon_type        varchar(64)
 );
