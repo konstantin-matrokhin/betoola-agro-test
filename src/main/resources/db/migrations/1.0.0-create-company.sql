d67e04a883a373c3e91d90b7388d95a33fbff0ac
Konstantin Matrokhin <remillary@gmail.com>
2021-05-16 14:21:12 +0700

    fix migraiton

--- src/main/resources/db/migrations/1.0.0-create-company.sql
+++ src/main/resources/db/migrations/1.0.0-create-company.sql
@@ -6,7 +6,7 @@ create table company
     id          uuid         not null default random_uuid(),
     address     varchar(128),
     city        varchar(128),
-    country     varchar(3) not null,
+    country     varchar(2) not null,
     fiscal_id   varchar(10),
     name        varchar(128),
     postal_code varchar(10)  not null
