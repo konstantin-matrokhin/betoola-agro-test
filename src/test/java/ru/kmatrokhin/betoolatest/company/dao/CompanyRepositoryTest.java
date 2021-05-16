3c5ed5bc9bb187eb4408f1b7ee2338f50a0c9ab6
Konstantin Matrokhin <remillary@gmail.com>
2021-05-16 18:32:20 +0700

    company repository tests

--- src/test/java/ru/kmatrokhin/betoolatest/company/dao/CompanyRepositoryTest.java
+++ src/test/java/ru/kmatrokhin/betoolatest/company/dao/CompanyRepositoryTest.java
@@ -5,9 +5,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.kmatrokhin.betoolatest.SpringTestBase;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @RequiredArgsConstructor(onConstructor_ = {@Autowired})
+@Transactional
 class CompanyRepositoryTest extends SpringTestBase {
   private final CompanyRepository companyRepository;
 
@@ -17,5 +23,29 @@ class CompanyRepositoryTest extends SpringTestBase {
     companyRepository.saveAndFlush(company);
     final var foundCompany = companyRepository.findById(company.getId());
     assertNotNull(foundCompany);
+  }
+
+  @Test
+  void findByIdAndDeletedDateNullTest() {
+    final var company = testCompany().setDeletedDate(LocalDateTime.now());
+    companyRepository.saveAndFlush(company);
+    assertTrue(companyRepository.findByIdAndDeletedDateNull(company.getId()).isEmpty());
+  }
+
+  @Test
+  void findByDeletedDateNullTest() {
+    final var companies = IntStream.range(0, 50).mapToObj(i -> {
+      System.out.println("company #" + i);
+      final var company = testCompany();
+      if (i % 10 == 0) {
+        company.setDeletedDate(LocalDateTime.now());
+      }
+      return company;
+    }).collect(Collectors.toList());
+    companyRepository.saveAll(companies);
+    companyRepository.flush();
+    final var byDeletedDateNull = companyRepository.findByDeletedDateNull();
+    assertEquals(companyRepository.findAll().size(), 50);
+    assertEquals(45, byDeletedDateNull.size());
   }
 }
