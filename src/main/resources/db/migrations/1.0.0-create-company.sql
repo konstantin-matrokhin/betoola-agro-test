ff00feb0e36fb1c00114f7fc28e0ff137c531779
Konstantin Matrokhin <remillary@gmail.com>
2021-05-16 16:20:52 +0700

    fix

--- src/main/resources/db/migrations/1.0.0-create-company.sql
+++ src/main/resources/db/migrations/1.0.0-create-company.sql
@@ -7,7 +7,7 @@ create table company
     address     varchar(128),
     city        varchar(128),
     country     varchar(2) not null,
-    fiscal_id   varchar(10),
+    fiscal_id   varchar(11),
     name        varchar(128),
-    postal_code varchar(255)  not null -- max length 10+?
+    postal_code varchar(10)  not null
 );
